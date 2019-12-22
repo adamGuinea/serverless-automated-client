e98854576264022336883c77500a2c0689b59250
adamGuinea <adamguinea22@gmail.com>
2019-12-22 11:36:56 +1000

    updating resources

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ const dev = {
   STRIPE_KEY: "pk_test_OMMnnbwMLR2If0crq5Xl25ym00aQbDPQKa",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-y7gj4lanfs8p"
+    BUCKET: "notes-app-2-api-dev-attachmentsbucket-6x7imrem1gzi"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://a0xwxcud6e.execute-api.us-east-1.amazonaws.com/dev"
+    URL: "https://krcrmj6z07.execute-api.us-east-2.amazonaws.com/dev"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_ScTF6HUtl",
-    APP_CLIENT_ID: "5ii2q26on0k79nj5su96v2k3iv",
-    IDENTITY_POOL_ID: "us-east-2:67a66cb7-97c7-4661-9a4f-85193163a993"
+    USER_POOL_ID: "us-east-2_vSNhhitrW",
+    APP_CLIENT_ID: "5ou3vmkp7bviqsvh3r9icgo5o0",
+    IDENTITY_POOL_ID: "us-east-2:9bb8439c-d953-487a-939f-d25b4b8b1087"
   },
   social: {
     FB: "305288310414976"
@@ -23,17 +23,17 @@ const prod = {
   STRIPE_KEY: "pk_test_OMMnnbwMLR2If0crq5Xl25ym00aQbDPQKa",
   s3: {
     REGION: "us-east-2",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-oap73zut2q8f"
+    BUCKET: "notes-app-2-api-prod-attachmentsbucket-172fdyva0cbnv"
   },
   apiGateway: {
     REGION: "us-east-2",
-    URL: "https://hd58q0nv57.execute-api.us-east-2.amazonaws.com/prod"
+    URL: "https://49fpjnomqk.execute-api.us-east-2.amazonaws.com/prod"
   },
   cognito: {
     REGION: "us-east-2",
-    USER_POOL_ID: "us-east-2_IQRZo4yH1",
-    APP_CLIENT_ID: "2k7nl2v93ho1travh05r9n9i71",
-    IDENTITY_POOL_ID: "us-east-2:eebb7faf-95df-48b1-be06-4f16d9b2db26"
+    USER_POOL_ID: "us-east-2_niGMT98QJ",
+    APP_CLIENT_ID: "1sced5991piqi5r2tbvi7mlt4i",
+    IDENTITY_POOL_ID: "us-east-2:3b138062-9088-4347-ada4-640812e15e54"
   },
   social: {
     FB: "305288310414976"
